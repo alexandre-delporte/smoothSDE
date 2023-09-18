cd145962351fa31175f28a7c6d242c52547be3da
Alexandre Delporte <Alexandre.delporte@univ-grenoble-alpes.fr>
2023-09-18 16:10:52 +0200

    Fix bug in setup to initialize random effects coeffs

--- R/sde.R
+++ R/sde.R
@@ -542,9 +542,9 @@ SDE <- R6Class(
                 coeff_re0=self$coeff_re()
                 #change to specific values according to 
                 #coeff_re0 in other_data if provided
-                if ("coeff_re0" %in% names(other_data)) {
-                  coeff_names=rownames(other_data$coeff_re0)
-                  coeff_re0[coeff_names,]=other_data$coeff_re0
+                if (!is.null(self$other_data()$coeff_re0)) {
+                  coeff_names=rownames(self$other_data()$coeff_re0)
+                  coeff_re0[coeff_names,]=self$other_data()$coeff_re0
                 }
                 tmb_par$coeff_re <- coeff_re0
                 tmb_par$log_lambda <- log(self$lambda())
