4343eb40e45e409a88d2fb3ee89e8189b1d11233
Alexandre Delporte <Alexandre.delporte@univ-grenoble-alpes.fr>
2023-10-26 10:50:46 +0200

    omitted to declare nllk_rcvm in file smoothSDE.cpp

--- src/smoothSDE.cpp
+++ src/smoothSDE.cpp
@@ -5,6 +5,7 @@
 #include "nllk/nllk_ou_ssm.hpp"
 #include "nllk/nllk_ctcrw.hpp"
 #include "nllk/nllk_e_seal_ssm.hpp"
+#include "nllk/nllk_rcvm.hpp"
 
 template<class Type>
 Type objective_function<Type>::operator() () {
