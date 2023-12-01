556b967c29ad7391d8f6ba0e63dca78158d442d8
Alexandre Delporte <alexdelporte83@gmail.com>
2023-12-01 15:25:01 +0100

    specify method 'BFGS' in the call to optim in the SDE class method fit

--- R/sde.R
+++ R/sde.R
@@ -773,7 +773,7 @@ SDE <- R6Class(
                 # Fit model
                 private$out_ <- optim(par = private$tmb_obj_$par,
                                       fn = private$tmb_obj_$fn, 
-                                      gr = private$tmb_obj_$gr,control=list(trace=1))
+                                      gr = private$tmb_obj_$gr,control=list(trace=1),method="BFGS")
                 # private$out_ <- do.call(optim, private$tmb_obj_)
             })
             private$out_$systime <- sys_time
