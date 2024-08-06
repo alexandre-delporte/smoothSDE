e9e50652cffab52808578242b5ad2cdb7c3fb21e
Alexandre Delporte <alexdelporte83@gmail.com>
2024-08-06 10:38:36 +0200

    remove nllk_racvm1.hpp

--- src/smoothSDE.cpp
+++ src/smoothSDE.cpp
@@ -6,7 +6,6 @@
 #include "nllk/nllk_ctcrw.hpp"
 #include "nllk/nllk_e_seal_ssm.hpp"
 #include "nllk/nllk_racvm.hpp"
-#include "nllk/nllk_racvm1.hpp"
 
 template<class Type>
 Type objective_function<Type>::operator() () {
